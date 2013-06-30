f65641ff2e00b5ec328c5abda6fe796c6817a18f
Olivier Grisel <olivier.grisel@ensta.org>
2013-06-29 23:11:42 -0500

    Slightly more parameterizable script

--- run_demo.py
+++ run_demo.py
@@ -1,5 +1,12 @@
 import provision
+import sys
 
-p = provision.Provisioner('ipython-azure-demo')
+if len(sys.argv) > 1:
+    service_name = sys.argv[1]
+else:
+    service_name = 'ipython-azure-demo'
+
+p = provision.Provisioner(sys.argv[1])
 p.launch_node(role_size='ExtraLarge')
+
 print('Demo launched on: %s.cloudapp.net' % p.service_name)
